4991ebec29e81b2d22098d242b45c64e79cf5b2d
unknown <skooter500>
2015-07-17 23:17:44 +0100

    Optimised build

--- buildabc.bat
+++ buildabc.bat
@@ -1,3 +1,3 @@
 cd src
-call emcc genmidi.c midifile.c parseabc.c parser2.c queues.c store.c -o ../abc2midi.js --pre-js wrapper.js -s EXPORTED_FUNCTIONS="['_main', '_abc2midiC']" -s INVOKE_RUN=0
+call emcc genmidi.c midifile.c parseabc.c parser2.c queues.c store.c -o ../abc2midi.js --pre-js wrapper.js -s EXPORTED_FUNCTIONS="['_main', '_abc2midiC']" -s INVOKE_RUN=0 -O3
 cd ..
